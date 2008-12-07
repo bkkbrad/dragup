c4117546198abdcf8aa960205cc24d8be3f27493
bkkbrad <bkkbrad@localhost>
2008-12-07 17:17:47 -0500

    (mostly) before refactoring into classes

--- dragup.rb
+++ dragup.rb
@@ -2,41 +2,57 @@
 require 'java'
 require 'yaml'
 require 'find'
-CONFIG = YAML::load(File.read(File.join(File.dirname(__FILE__), 'config.yaml')))
-$host = CONFIG['host']
-$user = CONFIG['user']
-$remote = CONFIG['remote-dir']
-$password = nil
-
-["TransferHandler","JSplitPane", "JTextField", "JPasswordField","JOptionPane", "JFrame", "JPanel", "JLabel", "JTextArea", "JButton", "JList", "BoxLayout", "JScrollPane", "JFileChooser", "JDialog"].each { |c| include_class "javax.swing.#{c}"}
+Dir.glob(File.join(File.dirname(__FILE__), "lib") + "/*.jar").each { |jar| require jar}
+import com.jcraft.jsch.JSch
+import org.jdesktop.swingworker.SwingWorker
+["ProgressMonitor", "TransferHandler","JSplitPane", "JTextField", "JPasswordField","JOptionPane", "JFrame", "JPanel", "JLabel", "JTextArea", "JButton", "JList", "BoxLayout", "JScrollPane", "JFileChooser", "JDialog"].each { |c| include_class "javax.swing.#{c}"}
 ["BorderLayout", "Frame"].each { |c| include_class "java.awt.#{c}" }
 include_class "java.awt.datatransfer.DataFlavor"
 
-#TODO do actual file transfer
-def transfer(f)
-  p [f, $host, $user, $remote]
-  true
-end
-frame = JFrame.new("Upload Files")
-frame.defaultCloseOperation=javax.swing.WindowConstants::EXIT_ON_CLOSE
-panel = JPanel.new() 
-panel.layout = BorderLayout.new
-frame.add(panel)
+class Dragup < JFrame
+  def initialize(host, user, remote, known_hosts)
+    @host = host
+    @user = user
+    @remote = remote
+    @known_hosts = known_hosts
+  
+    super("Upload Files to " + host)
+    default_close_operation = javax.swing.WindowConstants::EXIT_ON_CLOSE
 
-model = javax.swing.DefaultListModel.new
+    panel = JPanel.new() 
+    panel.layout = BorderLayout.new
+    add(panel)
 
-def add_path(model, index, base)
-  Find.find(File.expand_path(base)) do |path|
-    if FileTest.file?(path)
-      file_to_add = java.io.File.new(path)
-      if !model.contains(file_to_add)
-        model.add(index, file_to_add )
-        index += 1
+    @model = javax.swing.DefaultListModel.new
+  end
+
+  def absolute_path(path)
+    File.expand_path(path, File.dirname(__FILE__))
+  end
+  
+  def add_path(model, index, base)
+    Find.find(File.expand_path(base)) do |path|
+      if FileTest.file?(path)
+        file_to_add = java.io.File.new(path)
+        if !model.contains(file_to_add)
+          model.add(index, file_to_add )
+          index += 1
+        end
       end
     end
+    index
   end
-  index
 end
+
+CONFIG = YAML::load(File.read(absolute_path('config.yaml')))
+$host = CONFIG['host']
+$user = CONFIG['user']
+$remote = CONFIG['remote-dir']
+$known_hosts = absolute_path(CONFIG['known-hosts'])
+$password = nil
+
+
+
 
 list = JList.new(model)
 index = 0
@@ -103,36 +119,73 @@ remove_button.add_action_listener do |e|
   list.selected_indices.to_a.reverse.each { |i| model.remove_element_at(i) }
 end
 
+class UploadTask < SwingWorker 
+  def initialize(model, frame, pm)
+    @model = model
+    @frame = frame
+    @pm = pm
+    super()
+  end
+
+  def doInBackground
+    begin
+      log "Connecting to host #{$host} as #{$user}..."
+      jsch = JSch.new
+      session = jsch.get_session($user, $host);
+      session.password = $password
+      jsch.known_hosts = $known_hosts
+      session.connect
+      channel = session.open_channel("sftp")
+      channel.connect
+      log "Connected."
+      (0...@model.size()).each do |i|
+        @pm.set_progress(i)
+        puts "prgress" 
+        if @pm.is_canceled
+          @pm.close
+          raise "Transfer cancelled"
+        end
+        puts "not cancelled"
+        f = @model.get(0)
+        remote_name = File.join($remote, File.basename(f.path))
+        log "Transferring #{f} to #{remote_name}"
+        channel.put(java.io.FileInputStream.new(f.path), remote_name)
+        @model.remove_element_at(0)
+      end
+      log "Finished uploading files."
+      @pm.close
+    rescue Exception => e 
+      if f
+        error_string = "Error uploading file \"#{f}\":\n" + e.message
+      else
+        error_string = "Error connecting:\n" + e.message
+      end
+      log error_string
+      JOptionPane.showMessageDialog(@frame,"Transfer Error", error_string, JOptionPane::ERROR_MESSAGE)
+    end
+    log "Closing connection."
+    channel.exit if channel
+    session.disconnect if session
+  end
+end
 upload_button = JButton.new "Upload"
 upload_button.add_action_listener do |e|
   unless $password
-    hlabel = JLabel.new("Host:")
-    host_box = JTextField.new($host)
     ulabel = JLabel.new("User:")
     user_box = JTextField.new($user)
     plabel = JLabel.new("Password:")
     jpf = JPasswordField.new;
-    rlabel = JLabel.new("Remote directory:")
-    remote_box = JTextField.new($remote)
-    val = JOptionPane.showConfirmDialog(frame, [hlabel, host_box, ulabel, user_box, plabel, jpf, rlabel, remote_box].to_java, "Login information", JOptionPane::OK_CANCEL_OPTION);
+    val = JOptionPane.showConfirmDialog(frame, [ulabel, user_box, plabel, jpf].to_java, "Login information", JOptionPane::OK_CANCEL_OPTION);
     break unless val == JOptionPane::OK_OPTION
-    $host = host_box.text
-    $user = user_box.text
-    $remote = remote_box.text
-    $password = jpf.password
+    $user = user_box.text.strip
+    $password = ""
+    jpf.password.each { |c| $password << c }
   end
-  (0...model.size()).each do |i|
-    f = model.get(0)
-    log "Transfering #{f}"
-    unless transfer(f)
-      error_string = "Error uploading file \"#{f}\"."
-      JOptionPane.showMessageDialog(frame,"Transfer Error", error_string, JOptionPane::ERROR_MESSAGE)
-      log error_string
-      break
-    else
-      model.remove_element_at(0)
-    end
-  end
+  
+  f = nil
+  pm = ProgressMonitor.new(frame, "Transfering files", nil, 0, model.size())
+  task = UploadTask.new(model, frame, pm)
+  task.execute
 end
 
 panel.add(JLabel.new("Files to upload:"), BorderLayout::NORTH)
